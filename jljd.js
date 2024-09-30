9a6b65a5e8607316970b0a552778873a9b9be9dc
fallofftuners <falloff-tuners.0u@icloud.com>
2024-10-01 00:56:00 +0800

    Update jljd.js

--- jljd.js
+++ jljd.js
@@ -1,21 +1,16 @@
-/**
- * @fileoverview Example of HTTP rewrite.
- *
- * @supported Quantumult X (v1.0.5-build173)
- */
+/*
+酷我音乐 解锁会员试听及部分功能
 
-// $request, $response, $notify(title, subtitle, message), console.log(message)
-// $request.scheme, $request.method, $request.url, $request.path, $request.headers
-// $response.statusCode, $response.headers, $response.body
-//
-// $prefs is for persistent store and the data of $prefs will be cleared when Quantumult X is deleted.
-// $prefs.setValueForKey(value, key), $prefs.removeValueForKey(key), $prefs.removeAllValues(). Returns true or false, value and key should be string.
-// $prefs.valueForKey(key) returns value.
-//
-// setTimeout(function() { console.log("abc"); }, 1000);
-//
-// You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
-// Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/aawsdwd\.iimwcb0j5\.cc url script-response-body https://raw.githubusercontent.com/fallofftuners/scripts/refs/heads/main/jljd.js
+
+[mitm]
+hostname = aawsdwd.iimwcb0j5.cc
+
+**************************/
 
 var body = $response.body;
 var obj = JSON.parse(body);
@@ -235,4 +230,4 @@ body = JSON.stringify(obj);
 
 // console.log(body);
 
-$done(body);
+$done({body});

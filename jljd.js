8cde939c7be674cf22026196ab8baff4eab878bd
fallofftuners <falloff-tuners.0u@icloud.com>
2024-11-17 18:25:09 +0800

    Update jljd.js

--- jljd.js
+++ jljd.js
@@ -14,12 +14,11 @@ hostname = iimwcb0j5.cc
 let res = $response;
 
 
-console.log(res.status);
+ot=`Te=async()=>{try{const t=await G.mediaPlay({id:Number(B.currentRoute.value.query.id)});if(t.code===200)console.log(t.data),ue.value=t.data.collectInfo,o.value=t.data.mediaInfo,y.value=t.data.code,b.value=t.data.playable,$.value=t.data.watchCount,console.log(o.value.videoUrl);else return m(t.tip)}catch(t){return console.log(t)`;
+mt=`Te=async()=>{try{const t=await G.mediaPlay({id:Number(B.currentRoute.value.query.id)});if(t.code===200)t.data.playable=true,t.data.watchCount=999,t.data.mediaInfo.isBuy=true,t.data.collectInfo=0,t.data.mediaInfo.videoUrl=t.data.mediaInfo.videoUrl.replace("http://","https://"),console.log(t.data),ue.value=t.data.collectInfo,o.value=t.data.mediaInfo,y.value=t.data.code,b.value=t.data.playable,$.value=t.data.watchCount,console.log(o.value.videoUrl);else return m(t.tip)}catch(t){console.log(t)}}`;
+// console.log(res.status);
 console.log(res.body);
-if (res.status ===200){
-body = res.body;
 res.body = res.body.replace(ot,mt);
-}
 
 
 
